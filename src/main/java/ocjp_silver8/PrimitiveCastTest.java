e98b466113249855b1db495ade4ad9255455fdf2
yasu <yasu@dynamics>
2016-05-23 00:20:43 +0900

    - Added homogeneous & heterogenous overload sample  - Added some into README

--- src/main/java/ocjp_silver8/PrimitiveCastTest.java
+++ src/main/java/ocjp_silver8/PrimitiveCastTest.java
@@ -36,9 +36,18 @@ public class PrimitiveCastTest {
 		
 		
 	}
+	
+	public static void homogeneous(int i1, int i2){System.out.println("int int");};
+	public static void homogeneous(double d1, double d2){System.out.println("double double");};
+	
+	public static void heterogenous(double d, int i){System.out.println("int int");};
+	public static void heterogenous(int i, double d){System.out.println("double double");};
+
 	public static void main(String[] args) {
 		PrimitiveCastTest.testInteger();
 		PrimitiveCastTest.testFloat();
-
+		
+		PrimitiveCastTest.homogeneous(100, 200);  //実行可能。homogeneous(int,int)が呼ばれる。
+		//PrimitiveCastTest.heterogenous(100, 200); //コンパイルエラー！！あいまいな呼び出し。
 	}
 }
